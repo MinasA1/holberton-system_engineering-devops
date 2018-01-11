819e92ca00c378f1d69b28360c9d11016367f775
Minas <minasanton@gmail.com>
2018-01-11 09:07:13 +0000

    fix url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,10 @@ def top_ten(subreddit):
     """reddit API"""
     if type(subreddit) is not str:
         return 0
-    api = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     header = {'user-agent': 'MinasA1'}
     r = get(api, headers=header, allow_redirects=False)
     if r.status_code is 200:
         js = r.json()['data']['children']
         return '\n'.join([js[i]['data']['title']for i in range(10)])
+    return None;
