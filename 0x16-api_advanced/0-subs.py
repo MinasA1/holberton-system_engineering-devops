0bcc10f770a3ebf98596fd5fd492ee9bb4e1a8ef
Minas <minasanton@gmail.com>
2018-01-11 06:19:24 +0000

    Initial Commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""number_of_subscribers"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """reddit API"""
+    if type(subreddit) is not str:
+        return 0
+    api = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
+    header = {'user-agent': 'MinasA1'}
+    r = get(api, headers=header)
+    if r.status_code is 200:
+        return r.json()['data']['subscribers']
+    return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""top ten"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """reddit API"""
+    if type(subreddit) is not str:
+        return 0
+    api = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {'user-agent': 'MinasA1'}
+    r = get(api, headers=header)
+    if r.status_code is 200:
+        js = r.json()['data']['children']
+        return '\n'.join([js[i]['data']['title']for i in range(10)])
