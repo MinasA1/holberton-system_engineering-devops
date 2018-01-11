be6fb0df6ead4a59f19ee8be8f63f77be56cf8c7
Minas <minasanton@gmail.com>
2018-01-11 09:08:55 +0000

    removed semicolon

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,9 @@ def top_ten(subreddit):
     if type(subreddit) is not str:
         return 0
     api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    header = {'user-agent': 'MinasA1'}
+    header = {'user-agent': 'chrome'}
     r = get(api, headers=header, allow_redirects=False)
     if r.status_code is 200:
         js = r.json()['data']['children']
         return '\n'.join([js[i]['data']['title']for i in range(10)])
-    return None;
+    return None
